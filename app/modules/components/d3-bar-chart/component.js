4cd589ba500ff28c6297b3799456e1986b2d68d3
Faiz <891366402@qq.com>
2018-12-20 00:35:49 +0800

    基本拆分，明天全部参数化，有时间写hasblock

--- app/modules/components/d3-bar-chart/component.js
+++ app/modules/components/d3-bar-chart/component.js
@@ -1,36 +1,125 @@
 import Component from '@ember/component';
-import ChartInitial from './d3ChartInitial';
-
 import { run } from '@ember/runloop';
+import ChartInitial from './d3ChartInitial';
+import { select } from 'd3-selection';
+import { max } from 'd3';
 
 /*
     // 冒泡发送Event 用法与this.sendAction()相同
     this.get('eventHandle').upAction(this, 'actionName', ...args);
 */
-export default Component.extend(ChartInitial, {
-	// tagName: 'svg',
 
+const { keys, values } = Object;
+
+export default Component.extend(ChartInitial, {
 	width: 1024,
 	height: 768,
-	bgColor: '#000',
+	bgColor: '#f5faf8',
+	title: '基本封装',
+	subTitle: '',
+	isShowText: true,
 
-	// attributeBindings: ['width', 'height'],
+	localClassNames: 'd3-bar',
 
 	init() {
 		this._super(...arguments);
-		this.data = [];
+		this.data = {
+			'C++': 32,
+			'C#': 26,
+			'Java': 45,
+			'Python': 38,
+			'JavaScript': 53,
+			'R': 48,
+			'Scala': 42,
+			'Golang': 34,
+			'Swit': 37,
+			'Android': 36
+		};
+	},
+
+	initialize() {
+		let gridOption = null,
+			titleOption = null,
+			margin = null,
+			xScaleValues = keys(this.get('data')),
+			yScaleValues = [0, max(values(this.get('data')))],
+			xScaleOption = null,
+			xScale = null,
+			yScale = null,
+			mainChart = null,
+			mainChartOption = null,
+			mainChartTextOption = null;
+
+		margin = { top: 100, right: 20, bottom: 30, left: 30 };
+
+		gridOption = {
+			transform: `translate(${margin.left}, ${margin.top})`
+		};
+
+		titleOption = {
+			transform: `translate(${this.get('width') / 2}, -${margin.top / 2})`,
+			'text-anchor': 'middle',
+			'font-weight': 600
+		};
+
+		xScaleOption = {
+			transform: `translate(0, ${this.get('height') - margin.top - margin.bottom})`,
+			'font-weight': 'bold'
+		};
+
+		this.initChart(this.elementId);
+		this.setWidth(this.get('width'));
+		this.setHeight(this.get('height'));
+		this.setBackground(this.get('bgColor'));
+		this.setGrid(gridOption);
+
+		this.setTitle(this.get('title'), titleOption);
+		xScale = this.setXScale(
+			this.get('width') - margin.left - margin.right,
+			xScaleValues,
+			xScaleOption);
+		yScale = this.setYScale(
+			this.get('height') - margin.top - margin.bottom,
+			yScaleValues);
+
+
+		mainChartOption = {
+			class: 'bar',
+			width: xScale.bandwidth(),
+			height: (d) => { return this.get('mainChartHight') - yScale(d[1]); },
+			x: (d) => { return xScale(d[0]); },
+			y: (d) => { return yScale(d[1]); }
+		}
+
+		mainChartTextOption = {
+			class: 'barText',
+			'text-anchor': 'middle',
+			x: (d) => { return xScale(d[0]); },
+			y: (d) => { return yScale(d[1]); },
+			dx: xScale.bandwidth() / 2,
+			dy: 20
+		}
+
+		mainChart = this.drawMainChart(
+			keys(this.get('data')).map(key => [key, this.get('data')[key]]),
+			mainChartOption
+		);
+
+		if (this.get('isShowText')) {
+			this.isShowMainChartTipText(mainChart, mainChartTextOption);
+		}
+
+		mainChart.on('mouseover', function () {
+			select(this).attr('opacity', 0.7);
+		}).on('mouseout', function () {
+			select(this).attr('opacity', 1)
+		});
+
 	},
 
 	didReceiveAttrs() {
 		this._super(...arguments);
-		// let option = {
-		// 	bgColor: this.get('bgColor'),
-		// 	width: this.get('width'),
-		// 	height: this.get('height')
-		// }
-		run.scheduleOnce('render', this, this.initChart, this.elementId);
-		run.scheduleOnce('render', this, this.setWidth, 1024);
-		run.scheduleOnce('render', this, this.setHeight, 768);
+		run.scheduleOnce('render', this, this.initialize);
 	},
 
 	actions: {
--- app/modules/components/d3-bar-chart/d3ChartInitial.js
+++ app/modules/components/d3-bar-chart/d3ChartInitial.js
@@ -1,36 +1,95 @@
 import Mixin from '@ember/object/mixin';
 import { select } from 'd3-selection';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import { axisBottom, axisLeft } from 'd3-axis';
+
 
 const { keys } = Object;
+
+const setAttr = function (instance, option) {
+	keys(option).forEach(key => {
+		instance.attr(key, option[key]);
+	});
+}
 
 export default Mixin.create({
 	svgRoot: null,
 	gridContainer: null,
+	mainChartWidth: 0,
+	mainChartHight: 0,
 
 	initChart(elementId) {
-		window.console.info(keys)
 		this.set('svgRoot', select(`#${elementId}`).append('svg'));
 	},
 
-	setGrid(option) {
+	setGrid(option = {}) {
 		let gridInstance = this.get('svgRoot').append('g');
-		keys(option).forEach(key => {
-			gridInstance.attr(key, option[key]);
-		});
+
+		setAttr(gridInstance, option);
 		this.set('gridContainer', gridInstance);
 	},
 
 	setBackground(value) {
 		this.get('svgRoot').style('background-color', value);
 	},
+
 	setWidth(value) {
 		this.get('svgRoot').attr('width', value);
 	},
+
 	setHeight(value) {
 		this.get('svgRoot').attr('height', value);
 	},
 
-	setData(dataSource) {
+	setTitle(value, option = {}) {
+		let textInstance = this.get('gridContainer').append('text').text(value);
 
+		setAttr(textInstance, option);
+	},
+
+	setSubTitle(value, option = {}) {
+
+	},
+
+	isShowMainChartTipText(chart, option) {
+		let text = chart.append('text').text((d) => { return d[1]; });
+
+		setAttr(text, option);
+	},
+
+	setXScale(width, xValues, option = {}) {
+		let xScale = scaleBand().rangeRound([0, width]).padding(0.1),
+			xScaleInstance = this.get('gridContainer').append('g');
+
+		this.set('mainChartHight', width);
+		xScale.domain(xValues);
+
+		setAttr(xScaleInstance, option);
+
+		xScaleInstance.call(axisBottom(xScale))
+
+		return xScale;
+	},
+
+	setYScale(height, yValues, option = {}) {
+		let yScale = scaleLinear().rangeRound([height, 0]),
+			yScaleInstance = this.get('gridContainer').append('g');
+
+		this.set('mainChartHight', height);
+		yScale.domain(yValues);
+
+		setAttr(yScaleInstance, option);
+
+		yScaleInstance.call(axisLeft(yScale).ticks(10))
+		return yScale;
+	},
+
+	drawMainChart(dataSource, option) {
+		let chart = this.get('gridContainer').selectAll('bar').data(dataSource).enter().append('g'),
+			rect = chart.append('rect');
+
+		setAttr(rect, option);
+
+		return chart;
 	}
 });
